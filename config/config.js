0603c5a0bc54fdfa0bc773d804fae1147cf3abdc
darklegend <bang@trustingsocial.com>
2017-07-24 10:04:28 +0700

    TS

--- config/config.js
+++ config/config.js
@@ -5,6 +5,6 @@ const config = {
     askAgainTimeOut:'40000',
     closeDealTimeout:'65000',
     channel:'C6AK1Q2AW',
-    users:['U2ZPTV09M','U2MLLT1AP','U0DB7BT6H'],
+    users:['U2ZPTV09M','U2MLLT1AP','U0DB7BT6H','U04N0PGFA','U03DGC2HZ','U03DJFC8W'],
 };
 module.exports = config;
